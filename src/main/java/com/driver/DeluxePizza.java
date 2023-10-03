26b2aa4a3ab824234461b059b2176a3b8b994c8b
salman-sam23 <salmansam3855@gmail.com>
2023-10-03 20:26:43 +0530

    submit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,9 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        Pizza pza = new Pizza(isVeg);
+        pza.addExtraCheese();
+        pza.addExtraToppings();
         // your code goes here
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,11 +4,26 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private String bill = "";
+
+    private int extraCheese=0;
+
+    private int extraToppings=0;
+
+    private int takeAway=0;
+
+    private int totalSum = 0;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg)
+        {
+            price = 300;
+        }
+        else {
+            price = 400;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +32,45 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        extraCheese = 80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg)
+        {
+            extraToppings = 70;
+        }
+        else {
+            extraToppings = 120;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        takeAway = 20;
     }
 
     public String getBill(){
         // your code goes here
+        totalSum = price + extraCheese + extraToppings + takeAway;
+        if(price!=0)
+        {
+            bill = bill + "Base Price Of The Pizza: " + price + "\n";
+        }
+        if(extraCheese!=0)
+        {
+            bill = bill + "Extra Cheese Added: " + extraCheese + "\n";
+        }
+        if(extraToppings!=0)
+        {
+            bill = bill + "Extra Toppings Added: " + extraToppings + "\n";
+        }
+        if(takeAway!=0)
+        {
+            bill = bill + "Paperbag Added: " + takeAway + "\n";
+        }
+            bill = bill + "Total Price: " + totalSum + "\n";
         return this.bill;
     }
 }
